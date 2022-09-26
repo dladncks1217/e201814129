741c7db94be76dbff5d39bf1b481c22b1de7ac85
dladncks1217 <dlaxodud1217@gmail.com>
2022-09-26 17:22:45 +0900

    Main.java 수정 #1

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("hello world"); } }
+public class Main { public static void main(String[] args) { Hello hello = new Hello(); hello.say(); } }
