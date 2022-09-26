ddf712bc9f56cfb6936071b7a16ca25cdd80464d
dladncks1217 <dlaxodud1217@gmail.com>
2022-09-26 17:33:23 +0900

    Hello.java 수정 #1

--- Hello.java
+++ Hello.java
@@ -1,1 +1,1 @@
-public class Hello { String msg = "hello"; public void say() { System.out.println(msg); } }
+public class Hello { String msg = "hello"; public void say() { say(this.msg); } public void say(String msg) { System.out.println(msg); } }
